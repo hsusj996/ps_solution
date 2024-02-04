353a916a62a9ef7c5ae3f83848a7ccfc87c0b650
sejin <tpwls1355@naver.com>
2024-02-05 00:07:23 +0900

    20240205 add boj11048, 23290(x), 15655

--- ps_solution/src/baekjoon/prob23290.java
+++ ps_solution/src/baekjoon/prob23290.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -31,17 +32,29 @@ public class prob23290 {
   // }
 
   static cdnt sharkPos;
-  static List<Integer>[][] FishMap = new ArrayList[4][4];
-  static int[][] map = new int[4][4];
+  static List<Integer>[][] FishMap = new ArrayList[5][5];
+  static List<Integer>[][] FishMapTemp = new ArrayList[5][5];
+  static int[][] map = new int[5][5];
   static int ans = 0;
-  static int[] d_row = { 0, 0, -1, -1, -1, 0, 1, 1, 1 };
-  static int[] d_col = { 0, -1, -1, 0, 1, 1, 1, 0, -1 };
+  static int[] d8_row = { 0, 0, -1, -1, -1, 0, 1, 1, 1 };
+  static int[] d8_col = { 0, -1, -1, 0, 1, 1, 1, 0, -1 };
+  static int[] d4_row = { -1, 0, 1, 0 };
+  static int[] d4_col = { 0, -1, 0, 1 };
+  static int[] sharkRoute;
+  static int[] routeBuffer;
+  static int maxOfEatingFish = 0;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
     M = Integer.parseInt(st.nextToken());
     S = Integer.parseInt(st.nextToken());
+
+    for (int i = 1; i <= 4; i++) {
+      for (int j = 1; j <= 4; j++) {
+        FishMap[i][j] = new ArrayList<>();
+      }
+    }
 
     // 입력
     for (int i = 0; i < M; i++) {
@@ -62,8 +75,8 @@ public class prob23290 {
       Simulation();
     }
 
-    for (int i = 0; i < 4; i++) {
-      for (int j = 0; j < 4; j++) {
+    for (int i = 1; i <= 4; i++) {
+      for (int j = 1; j <= 4; j++) {
         ans += FishMap[i][j].size();
       }
     }
@@ -72,6 +85,13 @@ public class prob23290 {
   }
 
   private static void Simulation() {
+    for(int i=1;i<=4;i++){
+      for(int j=1;j<=4;j++){
+        FishMapTemp[i][j] = new ArrayList<>();
+        FishMapTemp[i][j].addAll(FishMap[i][j]);
+      }
+    }
+
     MoveFishes();
     MoveShark();
     UpdateMap();
@@ -79,39 +99,109 @@ public class prob23290 {
   }
 
   private static void DuplicateFishes() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'DuplicateFishes'");
-  }
-
-  private static void UpdateMap() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'UpdateMap'");
-  }
-
-  private static void MoveShark() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'MoveShark'");
-  }
-
-  private static void MoveFishes() {
-    for (int i = 0; i < 4; i++) {
-      for (int j = 0; j < 4; j++) {
-        for (int fish : FishMap[i][j]) {
-          for (int k = 0; k < 8; k++) {
-            int direction = (fish + k) % 8 == 0 ? 1 : (fish + k) % 8;
-            if (isPossible(i, j, direction)) {
-              FishMap[i + d_row[direction]][j + d_col[direction]].add(direction);
-              FishMap[i][j].remove(fish);
-            }
-          }
+    for (int i = 1; i <= 4; i++) {
+      for (int j = 1; j <= 4; j++) {
+        int num = FishMapTemp[i][j].size();
+        for (int k = 0; k < num; k++) {
+          FishMap[i][j].add(FishMapTemp[i][j].get(k));
         }
       }
     }
   }
 
+  private static void UpdateMap() {
+    for (int i = 1; i <= 4; i++) {
+      for (int j = 1; j <= 4; j++) {
+        if (map[i][j] > 0) {
+          map[i][j]--;
+        }
+      }
+    }
+  }
+
+  private static void MoveShark() {
+    map[sharkPos.row][sharkPos.col] = 0;
+
+    sharkRoute = new int[3];
+    routeBuffer = new int[3];
+    maxOfEatingFish = 0;
+    BackTrackingShark(sharkPos.row, sharkPos.col, 0, 0);
+
+    for (int i = 0; i < 3; i++) {
+      sharkPos.row += d4_row[sharkRoute[i]];
+      sharkPos.col += d4_col[sharkRoute[i]];
+
+      if(FishMap[sharkPos.row][sharkPos.col].size() > 0){
+        map[sharkPos.row][sharkPos.col] = 3;
+      }
+      FishMap[sharkPos.row][sharkPos.col].clear();
+    }
+    map[sharkPos.row][sharkPos.col] = -1;
+  }
+
+  private static void BackTrackingShark(int row, int col, int fishCnt, int depth) {
+    if (depth == 3) {
+      if (maxOfEatingFish < fishCnt) {
+        maxOfEatingFish = fishCnt;
+        sharkRoute = routeBuffer.clone();
+      }
+      return;
+    }
+
+    for (int i = 0; i < 4; i++) {
+      int new_row = row + d4_row[i];
+      int new_col = col + d4_col[i];
+
+      if (IsOutBound(new_row, new_col)) {
+        continue;
+      }
+
+      routeBuffer[depth] = i;
+      List<Integer> FishTempList = new ArrayList<>();
+      FishTempList.addAll(FishMap[new_row][new_col]);
+      int newPosFishCnt = FishMap[new_row][new_col].size();
+      FishMap[new_row][new_col].clear();
+      BackTrackingShark(new_row, new_col, fishCnt + newPosFishCnt, depth + 1);
+      FishMap[new_row][new_col] = FishTempList;
+    }
+  }
+
+  private static void MoveFishes() {
+    List<Integer>[][] NewMap = new ArrayList[5][5];
+    for (int i = 1; i <= 4; i++) {
+      for (int j = 1; j <= 4; j++) {
+        NewMap[i][j] = new ArrayList<>();
+      }
+    }
+
+    for (int i = 1; i <= 4; i++) {
+      for (int j = 1; j <= 4; j++) {
+        for (Iterator<Integer> iter = FishMap[i][j].iterator(); iter.hasNext();) {
+          int fish = iter.next();
+          for (int k = 8; k > 0; k--) {
+            int direction = (fish + k) % 8 == 0 ? 1 : (fish + k) % 8;
+            if (isPossible(i, j, direction)) {
+              NewMap[i + d8_row[direction]][j + d8_col[direction]].add(direction);
+              iter.remove();
+              break;
+            }
+          }
+        }
+      }
+    }
+
+    FishMap = NewMap;
+  }
+
   private static boolean isPossible(int row, int col, int direction) {
-    int new_row = row + d_row[direction];
-    int new_col
+    int new_row = row + d8_row[direction];
+    int new_col = col + d8_col[direction];
+
+    return !IsOutBound(new_row, new_col) && map[new_row][new_col] == 0;
+  }
+
+  private static boolean IsOutBound(int row, int col) {
+    return row < 1 || row > 4 || col < 1 || col > 4;
   }
 
 }
