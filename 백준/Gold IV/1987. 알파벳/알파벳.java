0dc080e208077ca1617d1898a997c38a1e0c04ae
hsusj996 <84006404+hsusj996@users.noreply.github.com>
2024-02-16 17:20:44 +0900

    [Gold IV] Title: 알파벳, Time: 1124 ms, Memory: 16240 KB -BaekjoonHub

--- 백준/Gold IV/1987. 알파벳/알파벳.java
+++ 백준/Gold IV/1987. 알파벳/알파벳.java
@@ -1,71 +1,60 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static class xy {
-		int x;
-		int y;
-
-		public xy(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
-
-	static int R;
-	static int C;
-	static char[][] board;
+	static int max = Integer.MIN_VALUE;
+	static char[][] map;
+	static boolean[][] visited;
+	static boolean[] alphaVisited;
+	static int R, C;
 	static int[] d_row = { -1, 0, 1, 0 };
 	static int[] d_col = { 0, 1, 0, -1 };
-	static boolean[] alpha_visited;
-	static int ans = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 
-		board = new char[R + 1][C + 1];
-		alpha_visited = new boolean[26];
-		for (int i = 1; i <= R; i++) {
-			String input = br.readLine();
+		map = new char[R][C];
+		visited = new boolean[R][C];
+		alphaVisited = new boolean[27];
+		for (int i = 0; i < R; i++) {
+			String s = br.readLine();
 			for (int j = 0; j < C; j++) {
-				board[i][j + 1] = input.charAt(j);
+				map[i][j] = s.charAt(j);
 			}
 		}
 
-		alpha_visited[board[1][1] - 'A'] = true;
-		dfs(new xy(1, 1), 1);
+		visited[0][0] = true;
+		alphaVisited[map[0][0] - 'A'] = true;
+		dfs(0, 0, 1);
 
-		System.out.println(ans);
+		System.out.println(max);
 	}
 
-	static void dfs(xy cdnt, int depth) {
-		boolean flag = false;
-
+	private static void dfs(int row, int col, int depth) {
+		max = Math.max(max, depth);
 		for (int i = 0; i < 4; i++) {
-			int new_x = cdnt.x + d_row[i];
-			int new_y = cdnt.y + d_col[i];
+			int newRow = row + d_row[i];
+			int newCol = col + d_col[i];
 
-			if (new_x <= 0 || new_x > R || new_y <= 0 || new_y > C) {
+			if (IsOutBound(newRow, newCol) || visited[newRow][newCol] || alphaVisited[map[newRow][newCol] - 'A']) {
 				continue;
 			}
 
-			if(alpha_visited[board[new_x][new_y] - 'A']) {
-				continue;
-			}
-			
-			alpha_visited[board[new_x][new_y] - 'A'] = true;
-			dfs(new xy(new_x, new_y), depth + 1);
-			alpha_visited[board[new_x][new_y] - 'A'] = false;
-			flag = true;
+			visited[newRow][newCol] = true;
+			alphaVisited[map[newRow][newCol] - 'A'] = true;
+			dfs(newRow, newCol, depth + 1);
+			visited[newRow][newCol] = false;
+			alphaVisited[map[newRow][newCol] - 'A'] = false;
 		}
+	}
 
-		if (!flag) {
-			ans = Math.max(ans, depth);
-		}
+	private static boolean IsOutBound(int row, int col) {
+		return row < 0 || row >= R || col < 0 || col >= C;
 	}
 
 }
