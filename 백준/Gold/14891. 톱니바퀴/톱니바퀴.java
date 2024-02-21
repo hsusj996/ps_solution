e84983cbcae2183bfa8ecc6abc8b52f4a998e13c
hsusj996 <84006404+hsusj996@users.noreply.github.com>
2024-02-21 16:37:46 +0900

    [Gold V] Title: 톱니바퀴, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -0,0 +1,96 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+  static List<Integer>[] gearList = new ArrayList[5];
+  static int K;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    for (int i = 1; i <= 4; i++) {
+      gearList[i] = new ArrayList<>();
+
+      String s = br.readLine();
+      for (int j = 0; j < 8; j++) {
+        gearList[i].add(s.charAt(j) - '0');
+      }
+    }
+
+    K = Integer.parseInt(br.readLine());
+    for (int i = 0; i < K; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int gearNum = Integer.parseInt(st.nextToken());
+      int direction = Integer.parseInt(st.nextToken());
+
+      if (gearNum - 1 > 0) {
+        if (gearList[gearNum - 1].get(2) != gearList[gearNum].get(6)) {
+          RotateGearLeft(gearNum - 1, direction * (-1));
+        }
+      }
+
+      if (gearNum + 1 <= 4) {
+        if (gearList[gearNum + 1].get(6) != gearList[gearNum].get(2)) {
+          RotateGearRight(gearNum + 1, direction * (-1));
+        }
+      }
+
+      if (direction == 1) {
+        int tmp = gearList[gearNum].get(7);
+        gearList[gearNum].remove(7);
+        gearList[gearNum].add(0, tmp);
+      } else {
+        int tmp = gearList[gearNum].get(0);
+        gearList[gearNum].remove(0);
+        gearList[gearNum].add(tmp);
+      }
+    }
+
+    int score = 0;
+    for (int i = 1; i <= 4; i++) {
+      score += (gearList[i].get(0)) * Math.pow(2, i - 1);
+    }
+
+    System.out.println(score);
+  }
+
+  private static void RotateGearRight(int gearNum, int direction) {
+    if (gearNum + 1 <= 4) {
+      if (gearList[gearNum + 1].get(6) != gearList[gearNum].get(2)) {
+        RotateGearRight(gearNum + 1, direction * (-1));
+      }
+    }
+
+    if (direction == 1) {
+      int tmp = gearList[gearNum].get(7);
+      gearList[gearNum].remove(7);
+      gearList[gearNum].add(0, tmp);
+    } else {
+      int tmp = gearList[gearNum].get(0);
+      gearList[gearNum].remove(0);
+      gearList[gearNum].add(tmp);
+    }
+  }
+
+  private static void RotateGearLeft(int gearNum, int direction) {
+    if (gearNum - 1 > 0) {
+      if (gearList[gearNum - 1].get(2) != gearList[gearNum].get(6)) {
+        RotateGearLeft(gearNum - 1, direction * (-1));
+      }
+    }
+
+    if (direction == 1) {
+      int tmp = gearList[gearNum].get(7);
+      gearList[gearNum].remove(7);
+      gearList[gearNum].add(0, tmp);
+    } else {
+      int tmp = gearList[gearNum].get(0);
+      gearList[gearNum].remove(0);
+      gearList[gearNum].add(tmp);
+    }
+  }
+}
