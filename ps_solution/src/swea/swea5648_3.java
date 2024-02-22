fe0015640f1907cd8b770496a2c0bad93c503142
sejin <tpwls1355@naver.com>
2024-02-22 22:16:00 +0900

    20240222 add swea5648

--- ps_solution/src/swea/swea5648_3.java
+++ ps_solution/src/swea/swea5648_3.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class swea5648_3 {
@@ -31,12 +33,13 @@ public class swea5648_3 {
 
     @Override
     public boolean equals(Object obj) {
+      if (!(obj instanceof xy)) {
+        return false;
+      }
+
       xy o = (xy) obj;
 
-      if (o.x == this.x && o.y == this.y) {
-        return true;
-      }
-      return false;
+      return this.x == o.x && this.y == o.y;
     }
 
   }
@@ -57,13 +60,13 @@ public class swea5648_3 {
     }
   }
 
-  static int[] d_x = { 1, -1, 0, 0 };
-  static int[] d_y = { 0, 0, -1, 1 };
+  static int[] d_x = { 0, 0, -1, 1 };
+  static int[] d_y = { 1, -1, 0, 0 };
   static int T, N;
   static StringBuilder result = new StringBuilder();
   static Atom[] atomArr;
-  static int[][] board;
-  static Map<xy, List<Integer>> collisionMap;
+  static Map<xy, Integer> collisionMap;
+  static Set<Integer> collisionSet;
   static int energySum;
   static int AtomCnt;
 
@@ -77,10 +80,8 @@ public class swea5648_3 {
       N = Integer.parseInt(br.readLine());
 
       atomArr = new Atom[N + 1];
-      board = new int[4001][4001];
       energySum = 0;
       AtomCnt = N;
-      int cnt = 4000;
 
       for (int i = 1; i <= N; i++) {
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -89,29 +90,24 @@ public class swea5648_3 {
         int direction = Integer.parseInt(st.nextToken());
         int energy = Integer.parseInt(st.nextToken());
 
-        atomArr[i] = new Atom(x * 2 + 2000, y * 2 + 2000, direction, energy);
-        board[x * 2 + 2000][y * 2 + 2000] = i;
+        atomArr[i] = new Atom(2 * (1000 + x), 2 * (1000 + y), direction, energy);
       }
 
-      while (cnt-- > 0) {
+      while (true) {
         if (AtomCnt <= 1) {
           break;
         }
         collisionMap = new HashMap<>();
+        collisionSet = new HashSet<>();
 
         MoveAtoms(); // 움직이기
 
         // 충돌
-        Iterator<xy> keys = collisionMap.keySet().iterator();
-        while (keys.hasNext()) {
-          xy cur = keys.next();
-
-          for (int atomNum : collisionMap.get(cur)) {
-            energySum += atomArr[atomNum].energy;
-            atomArr[atomNum].existed = false;
-            board[atomArr[atomNum].x][atomArr[atomNum].y] = 0;
-            AtomCnt--;
-          }
+        for (Iterator<Integer> iter = collisionSet.iterator(); iter.hasNext();) {
+          int atomNum = iter.next();
+          energySum += atomArr[atomNum].energy;
+          atomArr[atomNum].existed = false;
+          AtomCnt--;
         }
       }
 
@@ -127,8 +123,6 @@ public class swea5648_3 {
         continue;
       }
 
-      board[atomArr[i].x][atomArr[i].y] = 0;
-
       atomArr[i].x += d_x[atomArr[i].direction];
       atomArr[i].y += d_y[atomArr[i].direction];
 
@@ -138,17 +132,13 @@ public class swea5648_3 {
         continue;
       }
 
-      if (board[atomArr[i].x][atomArr[i].y] == 0) { // 0이라면 그냥 삽입
-        board[atomArr[i].x][atomArr[i].y] = i;
-      } else { // 0이 아니라면 충돌가능
-        if (collisionMap.containsKey(new xy(atomArr[i].x, atomArr[i].y))) { // 기존에 충돌 리스트가 있으면 삽입
-          collisionMap.get(new xy(atomArr[i].x, atomArr[i].y)).add(i);
-        } else { // 없으면 새로 원소 삽입
-          List<Integer> tmp = new ArrayList<>();
-          tmp.add(board[atomArr[i].x][atomArr[i].y]);
-          tmp.add(i);
-          collisionMap.put(new xy(atomArr[i].x, atomArr[i].y), tmp);
-        }
+      xy cdnt = new xy(atomArr[i].x, atomArr[i].y);
+
+      if (collisionMap.containsKey(cdnt)) {
+        collisionSet.add(i);
+        collisionSet.add(collisionMap.get(cdnt));
+      } else {
+        collisionMap.put(cdnt, i);
       }
     }
   }
