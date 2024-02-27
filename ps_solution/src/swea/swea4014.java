92d2ae92ed80c75cee5d9fb95980739ffd145061
sejin <tpwls1355@naver.com>
2024-02-28 00:25:54 +0900

    20240228 add

--- ps_solution/src/swea/swea4014.java
+++ ps_solution/src/swea/swea4014.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 public class swea4014 {
   static StringBuilder result = new StringBuilder();
   static StringTokenizer st = null;
-  static int T, N, X;
+  static int T, N, X, cnt;
   static int[][] board;
 
   public static void main(String[] args) throws NumberFormatException, IOException {
@@ -20,6 +20,7 @@ public class swea4014 {
       st = new StringTokenizer(br.readLine());
       N = Integer.parseInt(st.nextToken());
       X = Integer.parseInt(st.nextToken());
+      cnt = 0;
 
       board = new int[N][N];
       for (int i = 0; i < N; i++) {
@@ -29,24 +30,111 @@ public class swea4014 {
         }
       }
 
-      // 행 검사
-      for (int i = 0; i < N; i++) {
-        boolean flag = true;
-        int prev = board[i][0];
-        for (int j = 1; j < N; j++) {
-          if (prev == board[i][j])
-            continue;
+      CheckRow();
+      CheckCol();
 
-          if (Math.abs(prev - board[i][j]) >= 2) {
-            flag = false;
-            break;
-          }
+      result.append(cnt).append("\n");
+    }
 
-          if (prev < board[i][j]) {
+    System.out.println(result);
+  }
 
-          }
+  private static void CheckCol() {
+    boolean[][] airway = new boolean[N][N];
+    // 열 검사
+    for (int i = 0; i < N; i++) {
+      boolean flag = true;
+      int prev = board[0][i];
 
+      for (int j = 1; j < N; j++) {
+        if (!flag) {
+          break;
         }
+
+        if (prev == board[j][i]) {
+          continue;
+        }
+
+        if (Math.abs(prev - board[j][i]) >= 2) {
+          flag = false;
+          break;
+        }
+
+        if (prev < board[j][i]) {
+          airway[j - 1][i] = true;
+          for (int k = j - X; k < j - 1; k++) {
+            if (k < 0 || board[k][i] != board[j - 1][i] || airway[k][i]) {
+              flag = false;
+              break;
+            }
+            airway[k][i] = true;
+          }
+        }
+
+        if (prev > board[j][i]) {
+          airway[j][i] = true;
+          for (int k = j + X - 1; k > j; k--) {
+            if (k >= N || board[k][i] != board[j][i] || airway[k][i]) {
+              flag = false;
+              break;
+            }
+            airway[k][i] = true;
+          }
+        }
+        prev = board[j][i];
+      }
+      if (flag) {
+        cnt++;
+      }
+    }
+  }
+
+  private static void CheckRow() {
+    boolean[][] airway = new boolean[N][N];
+    // 행 검사
+    for (int i = 0; i < N; i++) {
+      boolean flag = true;
+      int prev = board[i][0];
+      for (int j = 1; j < N; j++) {
+        if (!flag) {
+          break;
+        }
+        if (prev == board[i][j]) {
+          continue;
+        }
+
+        if (Math.abs(prev - board[i][j]) >= 2) {
+          flag = false;
+          break;
+        }
+
+        if (prev < board[i][j]) {
+          airway[i][j - 1] = true;
+          for (int k = j - X; k < j - 1; k++) {
+            if (k < 0 || board[i][k] != board[i][j - 1] || airway[i][k]) {
+              flag = false;
+              break;
+            }
+            airway[i][k] = true;
+          }
+        }
+
+        if (prev > board[i][j]) {
+          airway[i][j] = true;
+          for (int k = j + X - 1; k > j; k--) {
+            if (k >= N || board[i][k] != board[i][j] || airway[i][k]) {
+              flag = false;
+              break;
+            }
+            airway[i][k] = true;
+          }
+        }
+
+        prev = board[i][j];
+      }
+
+      if (flag) {
+        cnt++;
       }
     }
   }
