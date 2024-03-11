f6912edb40fe773d305498654da87e207b3a1bc9
sejin <tpwls1355@naver.com>
2024-03-11 23:41:54 +0900

    20240311 add

---
+++ ps_solution/src/baekjoon/prob14391.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class prob14391 {
+    static StringBuilder result = new StringBuilder();
+    static StringTokenizer st = null;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        
+    }
+}
