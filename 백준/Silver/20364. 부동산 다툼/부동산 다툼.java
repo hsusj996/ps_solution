9f0c65c482a3ea3d4ae8ab9e9c792f2319d908fd
hsusj996 <84006404+hsusj996@users.noreply.github.com>
2024-04-06 22:13:45 +0900

    [Silver I] Title: 부동산 다툼, Time: 744 ms, Memory: 84756 KB -BaekjoonHub

---
+++ 백준/Silver/20364. 부동산 다툼/부동산 다툼.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    static HashSet<Integer> groundSet = new HashSet<>();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+        groundSet.add(1);
+
+        for (int i = 0; i < Q; i++) {
+            int x = Integer.parseInt(br.readLine());
+            boolean flag = false;
+            int blockNode = -1;
+            for (int cur = x; cur > 1; cur /= 2) {
+                if (groundSet.contains(cur)) {
+                    blockNode = cur;
+                    flag = true;
+                }
+            }
+
+            if (flag) {
+                sb.append(blockNode);
+            } else {
+                groundSet.add(x);
+                sb.append(0);
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
