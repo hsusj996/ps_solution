c514db8f34f5054c9d79c025fbed3750f2d879af
hsusj996 <tpwls1355@naver.com>
2024-04-17 20:53:28 +0900

    add

--- ps_solution/src/baekjoon/prob1271.java
+++ ps_solution/src/baekjoon/prob1271.java
@@ -1,14 +1,16 @@
 package baekjoon;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class prob1271 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    long n = sc.nextInt();
-    long m = sc.nextInt();
+    BigInteger n = new BigInteger(sc.next());
+    BigInteger m = new BigInteger(sc.next());
 
-    System.out.println(Math.abs(n - m));
+    System.out.println(n.divide(m));
+    System.out.println(n.mod(m));
   }
 }
