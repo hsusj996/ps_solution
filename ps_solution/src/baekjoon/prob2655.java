edd756a01e32658cb3bb077fc7b5a0378639ce09
sejin <tpwls1355@naver.com>
2024-04-24 17:56:38 +0900

    add

--- ps_solution/src/baekjoon/prob2655.java
+++ ps_solution/src/baekjoon/prob2655.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class prob2655 {
@@ -36,10 +37,11 @@ public class prob2655 {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = null;
     N = Integer.parseInt(br.readLine());
-    blockArr = new Block[N];
-    dpHeight = new int[N];
-    dpNo = new int[N];
-    for (int i = 0; i < N; i++) {
+    blockArr = new Block[N + 1];
+    blockArr[0] = new Block(0, 0, 0, 10001);
+    dpHeight = new int[N + 1];
+    dpNo = new int[N + 1];
+    for (int i = 1; i <= N; i++) {
       st = new StringTokenizer(br.readLine());
       blockArr[i] = new Block(i, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
           Integer.parseInt(st.nextToken()));
@@ -47,27 +49,51 @@ public class prob2655 {
 
     Arrays.sort(blockArr);
 
-    for (int i = 0; i < N; i++) {
-      dpNo[i] = dpFunc(i);
-    }
-
-    System.out.println(dpHeight[0]);
-  }
-
-  private static int dpFunc(int idx) {
-    if (dpNo[idx] != 0) {
-      return dpNo[idx];
-    }
-
-    for (int i = idx + 1; i < N; i++) {
-      if (blockArr[i].a < blockArr[idx].a) {
-        dpNo[idx] = dpFunc(idx);
-        dpHeight[idx] = blockArr[idx].h + dpFunc(i);
-        return i;
+    for (int i = 1; i <= N; i++) {
+      if (dpHeight[i] == 0) {
+        dpHeight[i] = dpFunc(i);
       }
     }
 
-    dpHeight[idx] = blockArr[idx].h;
-    return dpNo[idx] = -1;
+    int maxHeight = 0;
+    int maxIdx = 0;
+    for (int i = 1; i <= N; i++) {
+      if (dpHeight[i] > maxHeight) {
+        maxHeight = dpHeight[i];
+        maxIdx = i;
+      }
+    }
+
+    Stack<Integer> stk = new Stack<>();
+    for (int cur = maxIdx; cur != -1; cur = dpNo[cur]) {
+      stk.push(blockArr[cur].no);
+    }
+
+    System.out.println(stk.size());
+    while (!stk.isEmpty()) {
+      System.out.println(stk.pop());
+    }
+  }
+
+  private static int dpFunc(int idx) {
+    for (int i = idx + 1; i <= N; i++) {
+      if (blockArr[i].a < blockArr[idx].a) {
+        if (dpHeight[i] == 0) {
+          dpHeight[i] = dpFunc(i);
+        }
+
+        if (dpHeight[idx] < dpHeight[i] + blockArr[idx].h) {
+          dpNo[idx] = i;
+          dpHeight[idx] = dpHeight[i] + blockArr[idx].h;
+        }
+      }
+    }
+
+    if (dpNo[idx] != 0) {
+      return dpHeight[idx];
+    }
+
+    dpNo[idx] = -1;
+    return dpHeight[idx] = blockArr[idx].h;
   }
 }
